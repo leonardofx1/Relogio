15f772eee9a75e11b24f4453285ac76f07836d30
leonardofx1 <leonardonunesfreitas1@gmail.com>
2023-01-07 13:25:23 -0300

    'image dinâmica'

--- JS/relogio.js
+++ JS/relogio.js
@@ -1,4 +1,35 @@
 const display = document.querySelector('.display')
+const container = document.querySelector('.container')
+const img = document.querySelector('.img')
+
+
+
+console.log(container.style)
+const showTime = (template) => {
+    display.innerText = template
+}
+const checkTime = hour => {
+    if(hour > 5  && hour < 12 ) {
+     showImage('day')
+    }else if(hour >= 12 && hour <= 18 ){
+        showImage('evening')
+    }else {
+        showImage('night')
+    }
+}
+
+const showImage = time => {
+    img.setAttribute('src',`/img/${time}.jpg`)
+}
+
+const formattedText = (time) => {
+    const formatted = time.map(time =>  time < 10 ? `0${time} ` :  time )
+    const [hour, minute, second] = formatted
+    const templateString =  `${hour}:${minute}:${second}`
+
+     showTime(templateString)
+     checkTime(hour)
+}
 
 const getTime = () =>{
     const date= new Date
@@ -6,13 +37,9 @@ const getTime = () =>{
     const minutes = date.getMinutes()
     const seconds = date.getSeconds()
 
-    const hour =  hours < 10 ? `0${hours}`: hours
- 
-    const template  = `${hour} : ${minutes} : ${seconds}`;
-     display.innerText = template
+    const arrTime = [hours,minutes,seconds]
 
-    console.log(hours.length)
-
+    formattedText(arrTime)
 } 
 
 setInterval(getTime,1000)
